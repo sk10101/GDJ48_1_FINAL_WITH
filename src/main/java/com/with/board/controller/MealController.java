a54ca9f51ed68ebbe4cc91dc810fd416236d3fb1
kim,hyun-soo <rlagustn800@gmail.com>
2022-08-08 17:53:41 +0900

    식사 게시판 구현

--- src/main/java/com/with/board/controller/MealController.java
+++ src/main/java/com/with/board/controller/MealController.java
@@ -1,21 +1,78 @@
 package com.with.board.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.with.board.service.MealService;
 
 @Controller
 public class MealController {
 
 Logger logger = LoggerFactory.getLogger(this.getClass());
 	
-	@RequestMapping(value = "/memberMealMain.go", method = RequestMethod.GET)
-	public String memberMealMain() {
+	@Autowired MealService service;
+	
+	// 식사 메인페이지 이동
+	@RequestMapping(value = "/MealList", method = RequestMethod.GET)
+	public String memberMealMain(Model model) {
+		logger.info("식사 메인페이지 이동");
 		
+		ArrayList<com.with.board.dto.BoardDTO> list = service.list();
+		logger.info("리스트 개수 :"+list.size());
+		model.addAttribute("list", list);		
 		
-		
-		return "memberMealMain";
+		return "mealBoard/MealList";
 	}
+	
+	@RequestMapping(value = "/MealDetail", method = RequestMethod.GET)
+	public String detail(Model model, @RequestParam String board_idx) {
+		logger.info("식사 메인페이지 이동");
+
+		logger.info(board_idx+" 번 상세보기");
+		com.with.board.dto.BoardDTO bbs = service.detail(board_idx);
+		model.addAttribute("bbs", bbs);
+	
+		
+		return "mealBoard/MealDetail";
+	}
+	
+	
+	// 글 작성 페이지 이동
+	@RequestMapping(value = "/MealWrite.go")
+	public String writeForm( Model model,HttpSession session) {
+		logger.info("글쓰기 페이지 이동");
+			
+		return "mealBoard/MealWrite";
+	}
+	
+	// 글 작성 + 이미지 파일 업로드
+	@RequestMapping(value = "/MealWrite.do")
+	public String MealWrite(MultipartFile[] photos, Model model,HttpSession session,
+			@RequestParam HashMap<String, String> params,RedirectAttributes attr) {
+		
+			logger.info("params{}",params);
+			service.MealWrite(photos,params);
+			attr.addFlashAttribute("msg", "글 작성 완료.");
+			
+		
+		return "redirect:/list";
+	}
+	
+	
+	
+	
+	
 }
--- src/main/webapp/WEB-INF/views/mealBoard/MealWrite.jsp
+++ src/main/webapp/WEB-INF/views/mealBoard/MealWrite.jsp
@@ -33,7 +33,51 @@
 	   <jsp:include page="../commons/memberSideBar3.jsp"/>
 	   <div class="content">
 	       <!-- 여기에서 작업 시작하세요 -->
-	     
+	     <form action="MealWrite.do" method="post">
+	<h3>글쓰기</h3>
+		<table>
+			<tr>
+				<th>제목</th>
+				<td><input type="text" name="subject"  placeholder="30자 이내"/></td>
+			</tr>
+			<tr>
+				<th>내용</th>
+				<td><textarea name="content"  placeholder="500자 이내"></textarea></td>
+			</tr>
+			<tr>
+			<tr>
+				<th>성별</th>
+				<td><input type="checkbox" name="gender"/>남자만<input type="checkbox">여자만<input type="checkbox">상관없음</td>
+			</tr>
+			<tr>
+				<th>약속장소</th>
+				<td><input type="text" name="appoint_place"/> <input type="text" placeholder="주소 검색"></td>
+			</tr>
+			<tr>
+				<th>사진</th>
+				<td><input type="file" name="photo"/></td>
+			</tr>
+			<tr>
+				<th>모집인원</th>
+				<td>
+					<select name="option">
+						<option value="3">3</option>
+						<option value="3">2</option>
+						<option value="3">1</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<th>마감시간</th>
+				<td><input type="date" name="deadline"/></td>
+			</tr>
+			<tr>
+			<th colspan="2">
+				<button>등록</button>
+			</th>
+			</tr>
+		</table>
+	</form>
 	   </div>
 	</div>
 	<jsp:include page="../commons/footer.jsp"/>
