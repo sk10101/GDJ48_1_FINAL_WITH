bf951cf6905c0dcd48332ec48e78610456fb7704
Yang,Su-bin <subin1266@gmail.com>
2022-08-09 16:33:37 +0900

    2022-08-09 양수빈 작업

--- src/main/java/com/with/member/controller/MemberController.java
+++ src/main/java/com/with/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.with.member.controller;
 
+import java.lang.reflect.Array;
 import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,18 +22,45 @@ public class MemberController {
 	
 	@RequestMapping(value = "/myInfo")
 	public String myInfo(Model model) {
-		String idx = "tester";
-		HashMap<String, Object> map = service.mblist(idx);
+		String member_id = "tester";
+		HashMap<String, Object> map = service.mblist(member_id);
 		String name = service.univer(map.get("university_idx"));
+		int cnt = service.macnt(member_id);
+		cnt/=3;
+		float avg = cnt/3;
+		int yang[]= new int[4]; 
+		String subin[] = {"친절함","응답속도","시간약속"};
+		for(int i=0;i<3;i++) {
+			yang[i]+=service.average(member_id,subin[i]);
+			yang[3]+=yang[i];
+			logger.info("누적 값 : {}",yang[3]);
+		}
+		/*logger.info("전체 값 : ",yang[3]);
+		logger.info("친절함 : ",yang[0]);
+		logger.info("응답속도 : ",yang[1]);
+		logger.info("시간약속 : ",yang[2]);
+		avg = yang[3]/cnt;
+		logger.info("전체 평균 : ",avg);*/
 		map.put("university_idx",name);
+		map.put("manner_cnt",cnt);
 		model.addAttribute("mblist",map);
 		return "myPage/myInfo";
 	}
 	@RequestMapping(value = "/mbUpdate.go")
-	public String mbUpdate(Model model) {
+	public String mbUpgo(Model model) {
 		String idx = "tester";
 		HashMap<String, Object> map = service.mblist(idx);
 		model.addAttribute("mblist", map);
 		return "myPage/myInfoUpdate";
 	}
+	
+	@RequestMapping(value = "/mbUpdate.do")
+	public String mbUpdate(Model model, @RequestParam String member_pw,
+			@RequestParam String phone, @RequestParam int hide) {
+		String member_id = "tester";
+		service.update(member_pw,phone,hide,member_id);
+		return "myPage/myInfo";
+	}
+	
+	
 }
--- src/main/java/com/with/member/dao/MemberDAO.java
+++ src/main/java/com/with/member/dao/MemberDAO.java
@@ -8,4 +8,10 @@ public interface MemberDAO {
 
 	String univer(Object object);
 
+	void update(String member_pw, String phone, int hide, String member_id);
+
+	int macnt(String member_id);
+
+	int average(String member_id, String subin);
+
 }
